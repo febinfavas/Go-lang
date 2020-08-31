f860b9730edf72b138b1c0828a5aa407265e89d6
febinfavas <febinfavas414@gmail.com>
2020-08-31 13:58:01 +0530

    Postgres project

---
+++ project/table/students.sql
@@ -0,0 +1,7 @@
+CREATE TABLE students (student_id SERIAl,name VARCHAR(50),CGPA DECIMAL(4,2));
+
+INSERT INTO students(NAME,CGPA) VALUES ('febin',8.52);
+INSERT INTO students(NAME,CGPA) VALUES ('favas',10.00);
+
+SELECT * FROM students;
+
