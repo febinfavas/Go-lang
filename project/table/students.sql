2b578abf7ed7d52e11cbf9be2f3f100a1ea345c1
febinfavas <febinfavas414@gmail.com>
2020-09-04 16:39:04 +0530

    modified

--- project/table/students.sql
+++ project/table/students.sql
@@ -5,3 +5,10 @@ INSERT INTO students(NAME,CGPA) VALUES ('favas',10.00);
 
 SELECT * FROM students;
 
+INSERT INTO students(NAME,CGPA) VALUES ('ashiq',9.52);
+INSERT INTO students(NAME,CGPA) VALUES ('saleem',9.00);
+INSERT INTO students(NAME,CGPA) VALUES ('salin',9.80);
+INSERT INTO students(NAME,CGPA) VALUES ('farha',7.80);
+INSERT INTO students(NAME,CGPA) VALUES ('fahmi',8.80);
+
+select * from students;
