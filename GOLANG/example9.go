c8b88ff89e3eba7cc0bf5fc32bad961cd40eb71c
febinfavas <febinfavas414@gmail.com>
2020-11-20 12:27:22 +0530

    GO Examples

--- GOLANG/example9.go
+++ GOLANG/example9.go
@@ -6,7 +6,27 @@ func main() {
 
 	defer firstrun()
 	secondrun()
+
+	fmt.Println(div(3, 0))
+	fmt.Println(div(6, 3))
+	dempanic()
 }
 
 func firstrun()  { fmt.Println("I executed first") }
 func secondrun() { fmt.Println("I executed second") }
+
+func div(n1, n2 int) int {
+	defer func() {
+		fmt.Println(recover())
+	}()
+
+	return (n1 / n2)
+}
+
+func dempanic() {
+	defer func() {
+		fmt.Println(recover())
+	}()
+
+	panic("panic")
+}
